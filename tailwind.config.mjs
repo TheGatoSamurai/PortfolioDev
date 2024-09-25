3bb383956386df4c9bdb130651887ee3c7b64e5b
TheGatoSamurai <ge.quinenao@duocuc.cl>
2024-09-25 00:05:57 -0300

    Update september post18

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,14 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
+		screens: {
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+			'3xl': '1600px',
+		  },
 		extend: {},
 	},
 	plugins: [],
