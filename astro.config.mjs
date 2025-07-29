805f49576666b41468f86a32fd73ad529cfe3af0
TheGatoSamurai <ge.quinenao@duocuc.cl>
2025-07-28 23:10:57 -0400

    VitepluginStaticcopy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,19 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   output: 'server',
   adapter: netlify(),
   integrations: [tailwind()],
   vite: {
-    build: {
-      rollupOptions: {
-      }
-    }
-  },
-  hooks: {
-    'astro:build:done': async ({ dir }) => {
-      const fs = await import('fs/promises');
-      await fs.copyFile('_redirects', `${dir}/_redirects`);
-    }
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          { src: '_redirects', dest: '.' }
+        ]
+      })
+    ]
   }
 });
