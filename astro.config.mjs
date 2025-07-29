90f95ee77e705cd68a0ae48f6299611d94a7a1c3
TheGatoSamurai <ge.quinenao@duocuc.cl>
2025-07-28 23:32:29 -0400

    astro config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,6 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
-  integrations: [tailwind()],
-  vite: {
-    plugins: [
-      viteStaticCopy({
-        targets: [
-          { src: '_redirects', dest: '.' }
-        ]
-      })
-    ]
-  }
+  integrations: [tailwind()]
 });
